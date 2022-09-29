0e0b04261424fa3a359ec8e9a75806cab82ed18d
linzening <linzening@yeah.net>
2022-09-29 22:52:44 +0800

    第一个可用版本

--- xgo.go
+++ xgo.go
@@ -1,6 +1,17 @@
 package xgo
 
+import(	"os/exec")
 
-func (int) Add(a int,b int){
+func Add(a int,b int) int {
 	return a + b
+}
+// 获取系统的Uname
+func Uname() string {
+	cmd := exec.Command("uname")
+	out,err := cmd.Output()
+	if err != nil {
+		return "cmd error."
+	} else {
+		return string(out)
+	}
 }
